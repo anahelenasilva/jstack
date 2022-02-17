12ee627c7f9a6b76e47320c86fb6463336ad1fd1
Ana Helena <anahelenarp@hotmail.com>
2022-02-17 13:05:56 -0300

    chore: add express

---
+++ jstack-api/src/index.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (request, response) => {
+    response.send('Hello world')
+})
+
+app.listen(3000, console.log('Server running on http://localhost:3000'))
