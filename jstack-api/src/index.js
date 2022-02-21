451f4b484e60853aca30f944bb179f745a6080f9
Ana Helena <anahelenarp@hotmail.com>
2022-02-21 08:39:18 -0300

    chore: add initial code

--- jstack-api/src/index.js
+++ jstack-api/src/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (request, response) => {
-    response.send('Hello world')
+    response.send('Hello world!')
 })
 
 app.listen(3000, console.log('Server running on http://localhost:3000'))
